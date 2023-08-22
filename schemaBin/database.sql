ee46719ce2035d84d2a38dbe76986e91ee4002f2
Aditya Singh <adityasinghmpp@gmail.com>
2023-08-22 06:14:07 +0530

    Create MySQL Database and Table

---
+++ schemaBin/database.sql
@@ -0,0 +1,6 @@
+create database mobileapp;
+use mobileappdb;
+create table users (name varchar (50), email varchar (50), phone bigint, password varchar (50));
+desc users;
+select * from users;
+truncate table users;
