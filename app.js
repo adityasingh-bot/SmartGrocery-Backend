57aba54e40568536c405a9d64c7ca0375febd75e
= <=>
2023-08-26 18:41:32 +0530

    Aditya changes

--- app.js
+++ app.js
@@ -9,7 +9,8 @@ app.use("/users",users);
 app.get('/app', (req,res)=>{
     res.send("Hello I am here Node Express");
 })
-console.log("Shiv :::")
+console.log("Shiv :::");
+console.log("Aditya")
 app.listen(port, ()=>{
     console.log(`Running on ${port}`);
 })
